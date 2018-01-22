21253c4b21468e026d4bf0a08e9a6bac3b8d0c26
Freddie Ridell <freddie@codogo.io>
2018-01-22 17:28:55 +0000

    feat(parse): extract disk data from string

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,63 @@
 use std::{thread, time, fmt};
+use std::process::Command;
 
 const BACKGROUND : &str = "#272822";
-const WHITE : &'static str = "#eeeeee";
 const RED : &'static str = "#f92672";
-const ORANGE : &'static str = "#fd971f";
-const YELLOW : &'static str = "#e6db74";
-const GREEN : &'static str = "#a6e22e";
-const BLUE : &'static str = "#66d9ef";
-const PURPLE : &str = "#ae81ff";
+//const WHITE : &'static str = "#eeeeee";
+//const ORANGE : &'static str = "#fd971f";
+//const YELLOW : &'static str = "#e6db74";
+//const GREEN : &'static str = "#a6e22e";
+//const BLUE : &'static str = "#66d9ef";
+//const PURPLE : &str = "#ae81ff";
+
+fn get_disk_usage () -> String {
+    let output = Command::new("df")
+        .output().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+    });
+
+    if !output.status.success() {
+        return String::from("failure");
+    }
+
+    let disk_info_string= String::from_utf8_lossy(&output.stdout);
+    //Filesystem              1K-blocks      Used   Available   Use%   Mounted on
+    let lines = disk_info_string.split("\n");
+
+    let mut partitions_data = Vec::new();
+    let mut first = true;
+    for line in lines {
+        if first {
+            first = false;
+            continue;
+        }
+        let mut fields_itterator = line.split_whitespace();
+
+        //fieldsItterator.next();
+        //fieldsItterator.next();
+        let used = match fields_itterator.nth(2) {
+            Some(x) => x.parse::<i64>().unwrap(),
+            None => 0,
+        };
+        let size = match fields_itterator.nth(0) {
+            Some(x) => x.parse::<i64>().unwrap(),
+            None => 1,
+        };
+        let mounted_on = match fields_itterator.nth(1) {
+            Some(x) => x,
+            None => "___",
+        };
+        partitions_data.push( ( used, size, mounted_on ) );
+    }
+
+    println!("{:?}", partitions_data);
+
+    //let vec = split.collect::<Vec<&str>>();
+    // OR
+    //let vec: Vec<&str> = split.collect();
+
+    return String::from("return");
+}
 
 fn with_cmd(command: String, message: String) -> String {
     format!("%{{A:{}:}}{}%{{A}}", command, message)
@@ -22,7 +72,7 @@ fn with_fg(color: String, message: String) -> String {
 }
 
 fn render_left() -> String {
-    let acc = String::from("left");
+    let acc = get_disk_usage();
     return acc;
 }
 

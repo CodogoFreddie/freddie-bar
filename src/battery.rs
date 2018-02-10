38184e51478a5efa0b5e62c7afaa0f33e1f0c852
Freddie Ridell <freddie@codogo.io>
2018-02-10 16:38:03 +0000

    feat(battery): displaying with nice bar

--- src/battery.rs
+++ src/battery.rs
@@ -2,6 +2,27 @@ use std::process::Command;
 
 use render;
 
+fn render_battery ( state: &str, percentage: i64, time_to: String) -> String {
+    let text = String::from(format!(" {} {}% {} ", state, percentage, time_to));
+
+    let length = text.len() as i64;
+    let split_point = ( ( percentage * length ) / 100 ) as usize;
+
+    let first_half = text.chars().take(split_point).collect();
+    let second_half = text.chars().skip(split_point).collect();
+
+    let colored = format!(
+        " {}{} ",
+        render::with_fg(
+            render::BACKGROUND,
+            render::with_bg(render::GREEN, first_half)
+        ),
+        render::with_bg(render::RED, second_half)
+        );
+
+    return String::from(colored);
+}
+
 pub fn get() -> String {
     let output = Command::new("upower")
         .args(&["-d"])
@@ -13,36 +34,37 @@ pub fn get() -> String {
         return String::from("failure");
     }
 
-    let disk_info_string= String::from_utf8_lossy(&output.stdout);
-    //Filesystem              1K-blocks      Used   Available   Use%   Mounted on
-    let lines = disk_info_string.split("\n");
+    let battery_info_string= String::from_utf8_lossy(&output.stdout);
+    let lines = battery_info_string.split("\n");
 
-    let mut starts_with = "";
-    let mut percentage = "";
-    let mut time_to = "";
+    let mut state = "";
+    let mut percentage = 0;
+    let mut time_to = String::from("");
     for line in lines {
         if line.starts_with("    state:") {
-            starts_with = match line.split_whitespace().nth(1) {
+            state = match line.split_whitespace().nth(1) {
                 Some(x) => x,
                 None => ""
             };
         }
 
         if line.starts_with("    percentage") {
-            percentage = match line.split_whitespace().nth(1) {
-                Some(x) => x,
-                None => ""
-            };
+            let mut percentage_sting = String::from(
+                match line.split_whitespace().nth(1) {
+                    Some(x) => x,
+                    None => ""
+                }
+                );
+
+            percentage_sting.pop();
+
+            percentage = percentage_sting.parse::<i64>().unwrap();
         }
 
         if line.starts_with("    time to") {
-            time_to = line;
+            time_to = line.split_whitespace().skip(3).collect::<Vec<&str>>().join(" ");
         }
     }
 
-    println!( "sw {}", starts_with );
-    println!( "per {}", percentage );
-    println!( "tt {}", time_to);
-
-    return String::from("bat");
+    return render_battery(state, percentage, time_to);
 }
